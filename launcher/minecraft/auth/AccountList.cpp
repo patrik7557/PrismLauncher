098b0119c406112d994cc5d1aa1c16e3b4680e35
patrik7557 <46382355+patrik7557@users.noreply.github.com>
2025-08-12 16:28:52 +0200

    Update AccountList.cpp
    Signed-off-by: patrik7557 <46382355+patrik7557@users.noreply.github.com>

--- launcher/minecraft/auth/AccountList.cpp
+++ launcher/minecraft/auth/AccountList.cpp
@@ -562,9 +562,7 @@ void AccountList::setListFilePath(QString path, bool autosave)
 bool AccountList::anyAccountIsValid()
 {
     for (auto account : m_accounts) {
-        if (account->ownsMinecraft()) {
-            return true;
-        }
+        return true;
     }
     return false;
 }
